07415a16cecaeaf91b074f4c0953508fb8438074
rashmi ranjan <rsahoorashmi@gmail.com>
2026-08-01 11:27:39 +0530

    All setup Configured

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -49,16 +49,16 @@ const userSchema = new Schema({
 
 userSchema.pre("save", async function (next) {
     if (!this.isModified("password")) return next();
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
-userSchema.methods.isPasswordCoreect = async function (password) {
+userSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password)
 }
 
 userSchema.methods.generateAccessToken = async function () {
-    jwt.sign({
+    return jwt.sign({
         _id: this._id,
         email: this.email,
         username: this.username,
@@ -70,7 +70,7 @@ userSchema.methods.generateAccessToken = async function () {
         })
 }
 userSchema.methods.generateRefreshToken = async function () {
-    jwt.sign({
+    return jwt.sign({
         _id:this._id,
         
     }, process.env.REFRESH_ACCESS_TOKEN,
